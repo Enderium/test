4dd1a9f431ad98986569ed79ca94aad059df4575
Enderium <timo.kettenbach@outlook.de>
2019-03-12 20:15:50 +0100

    updated

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ class RechnerApp(App):
         button.text='Discord ist fertig'
         print("Das wars!")
 
+    def guilde(self,button,gui,app):
+        print("Guild Wars 2 wird ausgeführt!")
+        for x in [self,button,gui,app]:
+            print(x)
+        button.text='Guild Wars 2 ist fertig'
+        print("Das wars!")
+
 meineAnwendung=RechnerApp()
 print(meineAnwendung)
 RechnerApp().run()
