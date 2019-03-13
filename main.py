d1388e6f34bfdf3e3f1989a10345a3734bcead14
Enderium <timo.kettenbach@outlook.de>
2019-03-13 20:47:19 +0100

    updated

--- main.py
+++ main.py
@@ -4,26 +4,43 @@ from kivy.uix.button import Button
 class RechnerApp(App):
     pass
 
-    def berechne(self,button,gui,app):
-        print("LoL wird ausgeführt!")
+    def league(self,button,gui,app):
         for x in [self,button,gui,app]:
             print(x)
-        button.text='Das wars!'
-        print("Das wars!")
+        if button.text == 'LoL':
+            button.text='Das wars!'
+            print("Das wars!")
+        else:
+            button.text = 'LoL'
+
 
     def discorde(self,button,gui,app):
-        print("Discord wird ausgeführt!")
         for x in [self,button,gui,app]:
             print(x)
-        button.text='Das wars!'
-        print("Das wars!")
+        if button.text == 'Discord':
+            button.text='Das wars!'
+            print("Das wars!")
+        else:
+            button.text = 'Discord'
 
     def guilde(self,button,gui,app):
-        print("Guild Wars 2 wird ausgeführt!")
         for x in [self,button,gui,app]:
             print(x)
-        button.text='Das wars!'
-        print("Das wars!")
+        if button.text == 'Guild Wars 2':
+            button.text='Das wars!'
+            print("Das wars!")
+        else:
+            button.text = 'Guild Wars 2'
+
+    def button_pressed(self,button):
+        print(button.text)
+        print(self.root.ids)
+        if self.root.ids.outputLabel.text == 'Willkommen':
+            self.root.ids.outputLabel.text=button.text
+        elif button.text == 'Leertaste':
+            self.root.ids.outputLabel.text += ' '
+        else:
+            self.root.ids.outputLabel.text+= button.text
 
 meineAnwendung=RechnerApp()
 print(meineAnwendung)
